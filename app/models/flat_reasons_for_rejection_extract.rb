67ae5c2184a0328129759faba2e75a57d13185a3
JRG <47917431+JR-G@users.noreply.github.com>
2021-02-23 13:29:46 +0000

    WIP: application choice export uses new model

--- app/models/flat_reasons_for_rejection_extract.rb
+++ app/models/flat_reasons_for_rejection_extract.rb
@@ -1,19 +1,19 @@
 class FlatReasonsForRejectionExtract
   include ActiveModel::Model
 
-  def initialize(application_form)
-    @structured_rejection_reasons = application_form.application_choices.structured_rejection_reasons
+  def initialize(application_choice)
+    # @structured_rejection_reasons = application_form.application_choices.structured_rejection_reasons
+    @structured_rejection_reasons = application_choice.structured_rejection_reasons
   end
 
-  def structured_rejection_data(structured_rejection_reasons)
-  end
-
+  # def structured_rejection_data(structured_rejection_reasons)
+  # end
 
   # These three methods are copied from the application_choices_export.rb .... currently doesn't provide enough granularity
-  def format_structured_rejection_reasons(structured_rejection_reasons)
-    return nil if structured_rejection_reasons.blank?
+  def format_structured_rejection_reasons
+    return nil if @structured_rejection_reasons.blank?
 
-    select_high_level_rejection_reasons(structured_rejection_reasons)
+    select_high_level_rejection_reasons(@structured_rejection_reasons)
     .keys
     .map { |reason| format_reason(reason) }
     .join("\n")
