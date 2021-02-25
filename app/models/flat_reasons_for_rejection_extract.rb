7afd4731eaece702f21d71515fda92e543b9c16a
George Holborn <george.holborn@digtal.education.gov.uk>
2021-02-25 17:23:25 +0000

    WIP - adding methods to model and export file

--- app/models/flat_reasons_for_rejection_extract.rb
+++ app/models/flat_reasons_for_rejection_extract.rb
@@ -5,66 +5,70 @@ class FlatReasonsForRejectionExtract
     @structured_rejection_reasons = structured_rejection_reasons
   end
 
-  def top_level_reasons
-    I18n.t('reasons_for_rejection_copy.top_level_reasons').keys
-  end
-
-  def top_level_reason_title(reason)
-    I18n.t("reasons_for_rejection_copy.top_level_reasons.#{reason}.title")
-  end
-
-  def separate_high_level_rejection_reasons(structured_rejection_reasons)
-    select_high_level_rejection_reasons(structured_rejection_reasons)
-    .keys
-    .map { |reason| format_reason(reason) }
-  end
-
-  def candidate_behaviour
-    return nil if @structured_rejection_reasons.blank?
-    @structured_rejection_reasons.select { |reason, value| value == 'candidate_behaviour_y_n' }
-
-    select_high_level_rejection_reasons(structured_rejection_reasons)
-    .keys
-    .map { |reason| format_reason(reason) }
-  end
-
-  def qualifications
-
-  end
-
-  def quality_of_application
-
-
-  end
-
-  def quality_of_application_sub_reasons
-
-
-  end
-
   def candidate_behaviour?
-    return nil if @structured_rejection_reasons['candidate_behaviour_y_n'].blank?
-
-    @structured_rejection_reasons.select { |reason, value| value == 'Yes' && reason == 'candidate_behaviour_y_n' }.present?
+    top_level_reasons('candidate_behaviour_y_n')
   end
 
   def didnt_reply_to_interview_offer?
-    return nil if @structured_rejection_reasons['candidate_behaviour_what_did_the_candidate_do'].blank?
-
-    @structured_rejection_reasons['candidate_behaviour_what_did_the_candidate_do'].include?("didnt_reply_to_interview_offer")
+    sub_level_reasons('candidate_behaviour_what_did_the_candidate_do', 'didnt_reply_to_interview_offer')
   end
 
   def didnt_attend_interview?
-    return nil if @structured_rejection_reasons['candidate_behaviour_what_did_the_candidate_do'].blank?
-
-    @structured_rejection_reasons['candidate_behaviour_what_did_the_candidate_do'].include?("didnt_attend_interview")
+    sub_level_reasons('candidate_behaviour_what_did_the_candidate_do', 'didnt_attend_interview')
   end
 
   def candidate_behaviour_other_details
-    return nil if @structured_rejection_reasons['candidate_behaviour_other'].blank?
-
-    @structured_rejection_reasons['candidate_behaviour_other']
+    other_detail_or_what_to_improve('candidate_behaviour_other')
   end
+
+  def quality_of_application?
+    top_level_reasons('quality_of_application_y_n')
+  end
+
+  def personal_statement?
+    sub_level_reasons('quality_of_application_which_parts_needed_improvement', 'personal_statement')
+  end
+
+  def quality_of_application_personal_statement_what_to_improve
+    other_detail_or_what_to_improve('quality_of_application_personal_statement_what_to_improve')
+  end
+
+  def subject_knowledge?
+    sub_level_reasons('quality_of_application_which_parts_needed_improvement', 'subject_knowledge')
+  end
+
+  def quality_of_application_subject_knowledge_what_to_improve
+    other_detail_or_what_to_improve('quality_of_application_subject_knowledge_what_to_improve')
+  end
+
+  def quality_of_application_other_details
+    other_detail_or_what_to_improve('quality_of_application_other_details')
+  end
+
+  def qualifications?
+    top_level_reasons('qualifications_y_n')
+  end
+
+  def no_maths_gcse?
+    sub_level_reasons('qualifications_which_qualifications', 'no_maths_gcse')
+  end
+
+  def no_science_gcse?
+    sub_level_reasons('qualifications_which_qualifications', 'no_science_gcse')
+  end
+
+  def no_english_gcse?
+    sub_level_reasons('qualifications_which_qualifications', 'no_english_gcse')
+  end
+
+  def no_degree?
+    sub_level_reasons('qualifications_which_qualifications', 'no_degree')
+  end
+
+  def qualifications_other_details
+    other_detail_or_what_to_improve('qualifications_other_details')
+  end
+
 
 
   # These three methods are copied from the application_choices_export.rb .... currently doesn't provide enough granularity
@@ -85,5 +89,25 @@ class FlatReasonsForRejectionExtract
     reason
     .delete_suffix('_y_n')
     .humanize
+  end
+
+  private
+
+  def top_level_reasons(key)
+    return nil if @structured_rejection_reasons["#{key}"].blank?
+
+    @structured_rejection_reasons.select { |reason, value| value == 'Yes' && reason == "#{key}" }.present?
+  end
+
+  def sub_level_reasons(key, value)
+    return nil if @structured_rejection_reasons["#{key}"].blank?
+
+    @structured_rejection_reasons["#{key}"].include?("#{value}")
+  end
+
+  def other_detail_or_what_to_improve(key)
+    return nil if @structured_rejection_reasons["#{key}"].blank?
+
+    @structured_rejection_reasons["#{key}"]
   end
 end
--- app/services/support_interface/structured_reasons_for_rejection_export.rb
+++ app/services/support_interface/structured_reasons_for_rejection_export.rb
@@ -1,15 +1,26 @@
 module SupportInterface
   class StructuredReasonsForRejectionExport
     def data_for_export
-      data_for_export = application_forms.includes(:application_choices).each do |application_form|
-        structured_reasons_for_rejection = FlatReasonsForRejectionExtract.new(application_form.application_choices.rejected&.structured_rejection_reasons)
+      data_for_export = application_choices.map do |application_choice|
+        structured_reasons_for_rejection = FlatReasonsForRejectionExtract.new(application_choice.structured_rejection_reasons)
 
         output = {
           'Candidate behaviour' => structured_reasons_for_rejection.candidate_behaviour?,
           'Candidate behaviour - didnt_reply_to_interview_offer' => structured_reasons_for_rejection.didnt_reply_to_interview_offer?,
           'Candidate behaviour - didnt_attend_interview' => structured_reasons_for_rejection.didnt_attend_interview?,
           'Candidate behaviour - other detail' => structured_reasons_for_rejection.candidate_behaviour_other_details,
-
+          'Quality of application' => structured_reasons_for_rejection.quality_of_application?,
+          'Quality of application - personal statement' => structured_reasons_for_rejection.personal_statement?,
+          'Quality of application - personal statement details' => structured_reasons_for_rejection.quality_of_application_personal_statement_what_to_improve,
+          'Quality of application - subject knowledge' => structured_reasons_for_rejection.subject_knowledge?,
+          'Quality of application - subject knowledge details' => structured_reasons_for_rejection.quality_of_application_subject_knowledge_what_to_improve,
+          'Quality of application - other details' => structured_reasons_for_rejection.quality_of_application_other_details,
+          'Qualifications' => structured_reasons_for_rejection.qualifications?,
+          'Qualifications - no maths gcse' => structured_reasons_for_rejection.no_maths_gcse?,
+          'Qualifications - no science gcse' => structured_reasons_for_rejection.no_science_gcse?,
+          'Qualifications - no english gcse' => structured_reasons_for_rejection.no_english_gcse?,
+          'Qualifications - no degree' => structured_reasons_for_rejection.no_degree?,
+          'Qualifications - other detail' => structured_reasons_for_rejection.qualifications_other_details,
         }
 
         output
@@ -22,10 +33,8 @@ module SupportInterface
 
   private
 
-    def application_forms
-      ApplicationForm
-        .includes(:application_choices)
-        .where.not(equality_and_diversity: nil)
+    def application_choices
+      ApplicationChoice.where.not(structured_rejection_reasons: nil)
     end
   end
 end
