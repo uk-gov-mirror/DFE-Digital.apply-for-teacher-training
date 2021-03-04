d94abf6711d3ffc895c037a7e31beaf9a5363a7c
Aga Dufrat <a.dufrat@gmail.com>
2021-03-04 16:57:22 +0000

    Update all new configurable notification settings

--- app/controllers/provider_interface/notifications_controller.rb
+++ app/controllers/provider_interface/notifications_controller.rb
@@ -5,6 +5,13 @@ module ProviderInterface
         send_notifications: notification_params[:send_notifications],
       )
 
+      if FeatureFlag.active?(:configurable_provider_notifications)
+        UpdateAllNotificationPreferences.new(
+          current_provider_user.provider_user_notification,
+          value: notification_params[:send_notifications],
+        ).save!
+      end
+
       flash[:success] = 'Email notification settings saved'
       redirect_to provider_interface_notifications_path
     end
