992aeeed18b959dc25325e7521f60d9a437d4ae7
David Gisbey <davidgisbey@gmail.com>
2021-04-13 16:45:55 +0100

    Fix validation error when arriving from find with course params
    At the moment, if a candidate has an apply_2 application form
    with an application choice, when they arrive from Find
    with course params then they are told that they cannot
    add more than 3 courses assoicated with their application form.
    
    This commit checks to see if the application form is apply_1
    or apply_2 before rendering the correct error message.

--- app/controllers/candidate_interface/after_sign_in_controller.rb
+++ app/controllers/candidate_interface/after_sign_in_controller.rb
@@ -14,7 +14,11 @@ module CandidateInterface
         flash[:warning] = "You have already selected #{course.name_and_code}."
         redirect_to candidate_interface_course_choices_review_path
       elsif current_application.has_the_maximum_number_of_course_choices?
-        flash[:warning] = I18n.t('errors.messages.too_many_course_choices', course_name_and_code: course.name_and_code)
+        if current_application.apply_1?
+          flash[:warning] = I18n.t('errors.messages.too_many_course_choices', course_name_and_code: course.name_and_code)
+        else
+          flash[:warning] = I18n.t('errors.messages.apply_again_course_already_chosen', course_name_and_code: course.name_and_code)
+        end
         redirect_to candidate_interface_course_choices_review_path
       else
         redirect_to candidate_interface_course_confirm_selection_path(course.id)
